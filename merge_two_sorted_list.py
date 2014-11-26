3ef99485bfaace5c506f468125498f815aec562c
Rio Shen <rioxshen@gmail.com>
2014-11-26 15:37:01 -0500

    Update merge_two_sorted_list.py

--- merge_two_sorted_list.py
+++ merge_two_sorted_list.py
@@ -2,23 +2,6 @@ class Solution:
     # @param two ListNodes
     # @return a ListNode
     def mergeTwoLists(self, l1, l2):
-        """Merges two sorted list into one and returns the new head node.
-
-        Solution:
-            The general idea is merge sort, especially partition operation.
-            Traverse two sorted lists and compare their value, append the
-            smaller one onto the end of the result list.
-
-            We use a temporary dummy node as the head of the result list, and
-            a pointer *current* always points to the last node in the result
-            list.
-
-        Analysis:
-            Time Complexity: O(min(N, M)) where N and M means two lists
-            respectively.
-
-            Space Complexity: O(N+M)
-        """
         if not l1:
             return l2
         dummy = ListNode(-1)
