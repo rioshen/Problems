352a36574a48aca17ccb42bd9a475b019803f228
Rio Shen <rioxshen@gmail.com>
2014-11-26 18:38:14 -0500

    Update remove_duplicates_from_sorted_list.py

--- remove_duplicates_from_sorted_list.py
+++ remove_duplicates_from_sorted_list.py
@@ -8,18 +8,6 @@ class Solution:
     # @param head, a ListNode
     # @return a ListNode
     def deleteDuplicates(self, head):
-        """Remove duplicates from the list such that each element appears
-        only once.
-
-        Solution:
-            Traversing the list from the head node, while traversing compare
-            each node with its next node. If value of next node is same then
-            delete the next node.
-
-        Analysis:
-            Time Complexity: O(N) where N means the number of elements in the list
-            Space Complexity: O(1)
-        """
         if not head:
             return None
         curr = head
