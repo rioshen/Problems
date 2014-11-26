268947e177e2b59fd2f11db6e5e34f937578be84
Rio Shen <rioxshen@gmail.com>
2014-11-26 12:39:13 -0500

    Update binary_tree_inorder_traversal.py

--- binary_tree_inorder_traversal.py
+++ binary_tree_inorder_traversal.py
@@ -1,1 +1,28 @@
-#!/usr/bin/env python# Definition for a  binary tree node# class TreeNode:#     def __init__(self, x):#         self.val = x#         self.left = None#         self.right = Noneclass Solution:    # @param root, a tree node    # @return a list of lists of integers    def levelOrder(self, root):        if not root:            return [[]]        result, queue = [], collections.deque([root])        while queue:            sol = []            level = len(queue)            for i in xrange(level):                node = queue.popleft()                if node.left:                    queue.append(node.left)                if node.right:                    queue.append(node.right)                sol.append(node.val)            result.append(sol)        return result
+#!/usr/bin/env python
+
+# Definition for a  binary tree node
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param root, a tree node
+    # @return a list of lists of integers
+    def levelOrder(self, root):
+        if not root:
+            return [[]]
+        result, queue = [], collections.deque([root])
+        while queue:
+            sol = []
+            level = len(queue)
+            for i in xrange(level):
+                node = queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+                sol.append(node.val)
+            result.append(sol)
+        return result
