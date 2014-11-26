2f9474a850c40ee100a09b8d4299319f89190fc0
Rio Shen <rioxshen@gmail.com>
2014-11-26 12:40:35 -0500

    Update minimum_window_substring.py

--- minimum_window_substring.py
+++ minimum_window_substring.py
@@ -1,1 +1,9 @@
-#!/usr/bin/env pythonclass Solution:    # @return a string    def minWindow(self, S, T):        if not S or not T:            return ""        seen, target = {}, set(T)        lft, rgt = 0, len(S)-1
+#!/usr/bin/env python
+
+class Solution:
+    # @return a string
+    def minWindow(self, S, T):
+        if not S or not T:
+            return ""
+        seen, target = {}, set(T)
+        lft, rgt = 0, len(S)-1
