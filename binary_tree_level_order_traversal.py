4ca458934971257e331cee7010535235e66503f9
rioxshen <rioxshen@gmail.com>
2014-11-29 11:15:07 -0500

    update

---
+++ binary_tree_level_order_traversal.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+# Definition for a  binary tree node
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    # @param root, a tree node
+    # @return a list of lists of integers
+    def levelOrder(self, root):
+        if not root:
+            return []
+        result, queue = [], collections.deque([root])
+        while queue:
+            level, size = [], len(queue)
+            for i in xrange(size):
+                node = queue.popleft()
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+                level.append(node.val)
+            result.append(level)
+        return result
