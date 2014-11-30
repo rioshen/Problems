9726cbdacfe33800a568047f169a39a0fe40885c
rioxshen <rioxshen@gmail.com>
2014-11-30 14:29:11 -0500

    update

---
+++ 3sum_closest.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+class Solution:
+    # @return an integer
+    def threeSumClosest(self, num, target):
+        if not num:
+            return []
+        num.sort()
+        closest = 1 << 32
+        for i in xrange(len(num)-1):
+            lo, hi = i+1, len(num)-1
+            while lo < hi:
+                sums = num[i] + num[lo] + num[hi]
+                if sums == target:
+                    return sums
+                elif sums < target:
+                    lo += 1
+                else:
+                    hi -= 1
+                # compare which one is the closest to the target
+                if abs(sums-target) < abs(closest-target):
+                    closest = sums
+        return closest
