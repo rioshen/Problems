83e39cc73476b09945a11ace7355fb3e80550dd3
rioshen <rioxshen@gmail.com>
2014-12-21 07:47:36 -0500

    update

--- length_of_last_word.py
+++ length_of_last_word.py
@@ -2,4 +2,7 @@ class Solution:
     # @param s, a string
     # @return an integer
     def lengthOfLastWord(self, s):
-        return len(s.strip().split(' ')[-1])
+        if not s:
+            return 0
+        else:
+            return len(s.strip().split(' ')[-1])
--- remove_duplicates_from_sorted_list.py
+++ remove_duplicates_from_sorted_list.py
@@ -8,12 +8,14 @@ class Solution:
     # @param head, a ListNode
     # @return a ListNode
     def deleteDuplicates(self, head):
-        if not head:
+        if not head or not head.next:
             return None
+
         curr = head
         while curr and curr.next:
             if curr.val == curr.next.val:
                 curr.next = curr.next.next
             else:
                 curr = curr.next
+
         return head
