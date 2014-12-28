3813648d39f6dc4ceff3b3658c6afb74aca78d19
rioshen <rioxshen@gmail.com>
2014-12-28 11:46:12 -0500

    update

--- search_in_rotated_sorted_array_ii.py
+++ search_in_rotated_sorted_array_ii.py
@@ -3,4 +3,32 @@ class Solution:
     # @param target an integer
     # @return a boolean
     def search(self, A, target):
-        
+        if not A:
+            return False
+        lo, hi = 0, len(A)-1
+        while lo <= hi:
+            mid = (lo+hi) // 2
+            midval = A[mid]
+            if midval == target:
+                return True
+            elif A[lo] < midval:
+                if A[lo] <= target < midval:
+                    hi = mid
+                else:
+                    lo = mid + 1
+            elif A[lo] > midval:
+                if midval < target <= A[hi]:
+                    lo = mid + 1
+                else:
+                    hi = mid
+            else:
+                lo += 1
+        return False
+
+    def search(self, A, target):
+        if not A:
+            raise ValueError('wtf')
+        for elem in A:
+            if elem == target:
+                return True
+        return False
