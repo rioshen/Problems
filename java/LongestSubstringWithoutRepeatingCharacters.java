cb00f0917c356247efc74ca88cc52d303b633cee
rioshen <rioxshen@gmail.com>
2015-01-04 19:01:00 -0500

    update

---
+++ java/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,37 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by terry on 1/4/15.
+ */
+public class LongestSubstringWithoutRepeatingCharacters {
+    /**
+     *
+     * @param s
+     * @return
+     */
+    public int lengthOfLongestSubstring(String s) {
+        if (s == null || s.length() == 0) {
+            throw new IllegalArgumentException();
+        }
+
+        int length = 0;
+        int start = 0;
+        int end = 0;
+        Set<Character> seen = new HashSet<Character>();
+        while (end < s.length()) {
+            char content = s.charAt(end);
+            if (!seen.add(content)) { // we've visited this character
+                length = Math.max(length, end - start);
+                while (s.charAt(start) != content) {
+                    seen.remove(s.charAt(start));
+                    start++;
+                }
+                start++;
+            }
+            end++;
+        }
+
+        return Math.max(length, end - start);
+    }
+}
