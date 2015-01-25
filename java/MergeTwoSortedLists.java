5a4e623d626afebfb98258d4fac6fd922761dac6
rioshen <rioxshen@gmail.com>
2015-01-25 13:56:23 -0800

    update

---
+++ java/MergeTwoSortedLists.java
@@ -0,0 +1,30 @@
+/**
+ * Created by Robbert on 1/25/15.
+ * Merge Two Sorted Lists
+ * https://oj.leetcode.com/problems/merge-two-sorted-lists/
+ */
+public class MergeTwoSortedLists {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        }
+
+        ListNode result = new ListNode(Integer.MIN_VALUE);
+        ListNode current = result;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                current.next = l1;
+                l1 = l1.next;
+            } else {
+                current.next = l2;
+                l2 = l2.next;
+            }
+            current = current.next;
+        }
+
+        current.next = l1 == null ? l2 : l1;
+        return result.next;
+    }
+}
