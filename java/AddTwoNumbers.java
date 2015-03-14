e3d251514e47766268fe914857d223c387067881
Carl Shen <rioxshen@gmail.com>
2015-03-14 11:28:09 -0400

    Update AddTwoNumbers.java

--- java/AddTwoNumbers.java
+++ java/AddTwoNumbers.java
@@ -1,27 +1,21 @@
 /**
- * Created by Robbert on 1/4/15.
+ * Created by Rio on 1/4/15.
  * Add Two Numbers
  * https://oj.leetcode.com/problems/add-two-numbers/
  */
 public class AddTwoNumbers {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        if (l1 == null) {
-            return l2;
-        } else if (l2 == null) {
-            return l1;
-        }
-
-        ListNode result = new ListNode(Integer.MIN_VALUE);
+        ListNode result = new ListNode(0);
         ListNode current = result;
         int carry = 0;
+
         while (l1 != null || l2 != null) {
             int sums = carry;
             sums += l1 != null ? l1.val : 0;
             sums += l2 != null ? l2.val : 0;
 
             carry = sums / 10;
-            sums = sums % 10;
-            current.next = new ListNode(sums);
+            current.next = new ListNode(sums % 10);
 
             current = current.next;
             l1 = l1 == null ? null : l1.next;
